7d4362bd509be7263a77dd881be1f5e8e9afd67b
Alaneh Morrison <alanehmorrison96@gmail.com>
2018-08-16 14:23:08 -0400

    update

---
+++ source/repos/MrShaker/MrShaker.WebMVC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrShaker.WebMVC
+{
+    public class Class1
+    {
+    }
+}
